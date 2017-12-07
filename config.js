f1e127cd71bdd79f6bd1eb141fd60c5b283d604c
Nayaco <1194329594@qq,com>
2017-12-07 13:41:57 +0800

    Empty Object

--- config.js
+++
@@ -1,6 +0,0 @@
-exports.Expires={
-    fileMatch:/^(gif|png|jpg|js|css)$/ig,maxAge:606024365
-};
-exports.Compress={
-    match:/css|js|html/ig
-};
--- index.js
+++
@@ -1,8 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
-
-var handle = {}
-handle["/download"] = requestHandlers.download;
-handle["/upload"] = requestHandlers.upload;
-server.start(router.route, handle);
